c3802b54cf24cbf561df33526db4d14fe9394b77
unknown <liq543@gmail.com>
2023-08-30 10:22:26 -0500

    database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+// import { text } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -12,10 +13,42 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  // console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content })
+
+  const result = await request;
+
+  console.log('Data saved', result);
+
+};
+
+// Gets all the content from the database
+export const getDb = async () => {
+  console.log('GET to the database');
+  // console.error('getDb not implemented');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+
+  const result = await request;
+
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
